3b1a9656b6cf1b20fa4d7e14ec8eeccdb7388ba7
Mickey Mouse <Mickey.Mouse@softwire.com>
2022-07-11 11:10:24 +0100

    Allow King to move.

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -10,7 +10,38 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> squares = new List<Square>();
+            Square square = board.FindPiece(this);
+            int rowNum = square.Row + 1;
+            int colNum = square.Col + 1;
+            for (int i = -1; i < 2; i++)
+            {
+                AddSquare(ref squares, rowNum, colNum, ref square);
+                rowNum -= 1;
+            }
+
+            colNum = square.Col - 1;
+            rowNum = square.Row + 1;
+            for (int i = -1; i < 2; i++)
+            {
+                AddSquare(ref squares, rowNum, colNum, ref square);
+                rowNum -= 1;
+            }
+            AddSquare(ref squares, square.Row + 1, square.Col, ref square);
+            AddSquare(ref squares, square.Row - 1, square.Col, ref square);
+            return squares;
+        }
+        private void AddSquare(ref List<Square> squares, int rowNum, int colNum, ref Square currentSquare)
+        {
+            if (rowNum == currentSquare.Row && colNum == currentSquare.Col)
+            {
+                return;
+            }
+
+            if (rowNum >= 0 && rowNum < 8 && colNum >= 0 && colNum < 8)
+            {
+                squares.Add(new Square(rowNum, colNum));
+            }
         }
     }
 }
