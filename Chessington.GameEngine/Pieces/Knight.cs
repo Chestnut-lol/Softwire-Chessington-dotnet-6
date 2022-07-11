9b631202addb707991c9662e34b28a7d03483b8d
Mickey Mouse <Mickey.Mouse@softwire.com>
2022-07-11 12:13:01 +0100

    Disallow knights to take friendly pieces.

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -10,37 +11,61 @@ namespace Chessington.GameEngine.Pieces
         {
         }
 
+
+
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
             List<Square> squares = new List<Square>();
             Square square = board.FindPiece(this);
             int addRow = 2;
             int addCol = 1;
-            AddSquare(ref squares, square.Row + addRow, square.Col + addCol, ref square);
-            AddSquare(ref squares, square.Row - addRow, square.Col - addCol, ref square);
-            AddSquare(ref squares, square.Row - addRow, square.Col + addCol, ref square);
-            AddSquare(ref squares, square.Row + addRow, square.Col - addCol, ref square);
+            AddSquare(ref squares, square.Row + addRow, square.Col + addCol, ref square, ref board);
+            AddSquare(ref squares, square.Row - addRow, square.Col - addCol, ref square, ref board);
+            AddSquare(ref squares, square.Row - addRow, square.Col + addCol, ref square, ref board);
+            AddSquare(ref squares, square.Row + addRow, square.Col - addCol, ref square, ref board);
             addRow = 1;
             addCol = 2;
-            AddSquare(ref squares, square.Row + addRow, square.Col + addCol, ref square);
-            AddSquare(ref squares, square.Row - addRow, square.Col - addCol, ref square);
-            AddSquare(ref squares, square.Row - addRow, square.Col + addCol, ref square);
-            AddSquare(ref squares, square.Row + addRow, square.Col - addCol, ref square);
+            AddSquare(ref squares, square.Row + addRow, square.Col + addCol, ref square, ref board);
+            AddSquare(ref squares, square.Row - addRow, square.Col - addCol, ref square, ref board);
+            AddSquare(ref squares, square.Row - addRow, square.Col + addCol, ref square, ref board);
+            AddSquare(ref squares, square.Row + addRow, square.Col - addCol, ref square, ref board);
 
 
             return squares;
         }
+        private bool CheckValidSquare(int rowNum, int colNum)
+        {
+            return (rowNum >= 0 && rowNum < 8 && colNum >= 0 && colNum < 8);
+        }
 
-        private void AddSquare(ref List<Square> squares, int rowNum, int colNum, ref Square currentSquare)
+        private bool CheckSquareTakenByFriendly(int rowNum, int colNum, ref Board board)
+        {
+            if (!CheckValidSquare(rowNum, colNum))
+            {
+                throw new Exception("Invalid rowNum and/or colNum!");
+            }
+
+            var piece = board.GetPiece(new Square(rowNum, colNum));
+            if ((piece == null) || (piece.Player != this.Player))
+            {
+                return false;
+            }
+
+            return true;
+        }
+        private void AddSquare(ref List<Square> squares, int rowNum, int colNum, ref Square currentSquare, ref Board board)
         {
             if (rowNum == currentSquare.Row && colNum == currentSquare.Col)
             {
                 return;
             }
 
-            if (rowNum >= 0 && rowNum < 8 && colNum >= 0 && colNum < 8)
+            if (CheckValidSquare(rowNum, colNum))
             {
-                squares.Add(new Square(rowNum, colNum));
+                if (!CheckSquareTakenByFriendly(rowNum, colNum, ref board))
+                {
+                    squares.Add(new Square(rowNum, colNum));
+                }
             }
         }
     }
