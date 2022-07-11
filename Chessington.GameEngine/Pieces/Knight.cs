1eb9c6dd10b8d7a861d0a6a0db5d50fb3c7f315d
Mickey Mouse <Mickey.Mouse@softwire.com>
2022-07-11 11:01:49 +0100

    Allow knights to move.

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -6,11 +6,43 @@ namespace Chessington.GameEngine.Pieces
     public class Knight : Piece
     {
         public Knight(Player player)
-            : base(player) { }
+            : base(player)
+        {
+        }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> squares = new List<Square>();
+            Square square = board.FindPiece(this);
+            int addRow = 2;
+            int addCol = 1;
+            AddSquare(ref squares, square.Row + addRow, square.Col + addCol, ref square);
+            AddSquare(ref squares, square.Row - addRow, square.Col - addCol, ref square);
+            AddSquare(ref squares, square.Row - addRow, square.Col + addCol, ref square);
+            AddSquare(ref squares, square.Row + addRow, square.Col - addCol, ref square);
+            addRow = 1;
+            addCol = 2;
+            AddSquare(ref squares, square.Row + addRow, square.Col + addCol, ref square);
+            AddSquare(ref squares, square.Row - addRow, square.Col - addCol, ref square);
+            AddSquare(ref squares, square.Row - addRow, square.Col + addCol, ref square);
+            AddSquare(ref squares, square.Row + addRow, square.Col - addCol, ref square);
+
+
+            return squares;
+        }
+
+        private void AddSquare(ref List<Square> squares, int rowNum, int colNum, ref Square currentSquare)
+        {
+            if (rowNum == currentSquare.Row && colNum == currentSquare.Col)
+            {
+                return;
+            }
+
+            if (rowNum >= 0 && rowNum < 8 && colNum >= 0 && colNum < 8)
+            {
+                squares.Add(new Square(rowNum, colNum));
+            }
         }
     }
 }
+    
